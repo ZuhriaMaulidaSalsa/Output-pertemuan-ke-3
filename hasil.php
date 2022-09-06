6bcec8c013d8fb80e44935f02a7e9c74fdd0f509
zuhriasalsa <111842759+zuhriasalsa@users.noreply.github.com>
2022-09-06 19:40:23 +0700

    Update hasil.php

--- hasil.php
+++ hasil.php
@@ -14,8 +14,9 @@ $terupload = move_uploaded_file($sementara, $upload.$file);
 if ($terupload) {
 	echo "Nama : " . $_POST['Nama'];
 	echo "<br> Nim : " . $_POST['NIM'];
-    echo "<br> Upload berhasil!";
-    echo "<br> Link: <a href='".$upload.$file."'>".$file."</a>";
+   	echo "<br> Upload berhasil!";
+    	echo "<br> Link: <a href='".$upload.$file."'>".$file."</a>";
+	echo "<br> foto diri : <br> <img src='".$upload.$file."' alt='' width='200' >";
 } else {
     echo "Upload Gagal!";
 }
